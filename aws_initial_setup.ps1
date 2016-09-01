c64e6919862c5f22c3c59ccbf92914e0f80b7cb5
jplebre <jplebre@gmail.com>
2016-09-01 21:58:06 +0100

    Consolidating powershell scripts to conform to conventions

--- aws_initial_setup.ps1
+++ aws_initial_setup.ps1
@@ -6,13 +6,13 @@ $directory = Split-Path -Parent $MyInvocation.MyCommand.Path
 # Create EC2 SSH key-pair
 aws ec2 create-key-pair --key-name speakr.keypair > $directory/ec2_keypair.txt
 aws s3 cp ec2_keypair.txt s3://speakr-keypairs
+Write-Host "EC2 KeyPair saved to s3"
 
 # Create Route53 Hosted Zone
 aws route53 create-hosted-zone --name speakr.rocks --caller-reference speakr.rocks_01 > $directory/hosted_zone_info.json
 $HostedZoneId = (Get-Content "$directory/hosted_zone_info.json" -Raw) | ConvertFrom-Json
 
 # Boot up services stacks
-# Call another script that loops through Components and runs the scripts there 
+# TODO: Call another script that loops through Components and runs the scripts there 
 
 Write-Host "Please manually copy AWS Key and AWS Secret for TravisCI to your TravisCI environment"
-Write-Host "EC2 KeyPair saved to directory"
