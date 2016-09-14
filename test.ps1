b0ac3b188e2909aae5783f10350736dad3f3a855
jplebre <jplebre@gmail.com>
2016-09-14 19:26:40 +0100

    Minor improvements

--- test.ps1
+++
@@ -1,7 +0,0 @@
-$directory = Split-Path -Parent $MyInvocation.MyCommand.Path
-
-# Create database (MySQL RDS)
-"Username: dbadmin`n" > $directory/db_password.txt
-& "$directory/EnvironmentSetup/database_setup/rds_setup.ps1" >> $directory/db_password.txt
-aws s3 cp db_password.txt s3://speakr-keypairs
-Write-Host "DB Password saved to s3"
